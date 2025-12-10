86e3b13a94f04f11d7b89913d3b9ee67e3820f90
seren.kim <seren.kim@kakaomobility.com>
2025-12-10 15:10:57 +0900

    push

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -11,6 +11,7 @@ export default function ChatsPage() {
 
     useEffect(() => {
         if (!data) return;
+        
 
         const fetchUnreadStatus = async () => {
             // Fetch latest messages for rooms
